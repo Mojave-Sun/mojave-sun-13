2b9d099c3552bf6805040ccb56ce35efa44f290f
Koshenko <53068134+Koshenko@users.noreply.github.com>
2022-01-09 16:30:41 -0700

    Bumps version (#1968)

--- dependencies.sh
+++ dependencies.sh
@@ -15,7 +15,7 @@ export NODE_VERSION=16
 export NODE_VERSION_PRECISE=16.13.1
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.1
+export SPACEMAN_DMM_VERSION=suite-1.7.2
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.6.8
