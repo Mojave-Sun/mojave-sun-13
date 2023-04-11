17f9793c397dceb7c94f401a47d6f76d3d626cb2
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2023-04-11 13:00:18 -0400

    Converts core tgui to ts (#74638)
    ## About The Pull Request
    Converts the remaining core tgui files into typescript
    Builds tests for these files
    Completed some todos to rewrite/refactor functions
    ## Why It's Good For The Game
    Typescript good
    ## Changelog
    N/A nothing player facing
    
    ---------
    
    Co-authored-by: Jeremiah <jlsnow301@pm.me>

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -27,13 +27,14 @@ import './styles/themes/wizard.scss';
 import './styles/themes/admin.scss';
 import './styles/themes/mojave.scss'; // MOJAVE SUN EDIT -TGUI
 
-import { perf } from 'common/perf';
-import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
-import { setupHotKeys } from './hotkeys';
+import { StoreProvider, configureStore } from './store';
+
 import { captureExternalLinks } from './links';
 import { createRenderer } from './renderer';
-import { configureStore, StoreProvider } from './store';
+import { perf } from 'common/perf';
 import { setupGlobalEvents } from './events';
+import { setupHotKeys } from './hotkeys';
+import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
 
 perf.mark('inception', window.performance?.timing?.navigationStart);
 perf.mark('init');
